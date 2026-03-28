568ffac4d7a0929d6d9a9699a411c4a997cab513
danilosouzah60-svg <195672445+RuanDsouzah@users.noreply.github.com>
2026-03-27 23:21:27 -0300

    aulas e exercicios
    fiz o primeiro exercicio de js de interatividade sobre nacionalidade

---
+++ aula-011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`a velocidade do seu carro estar em ${vel}km/h`)
+if(vel > 60){
+    console.log(`você ultrapassou a velocidade permitida!MULTADO!`)
+}
+console.log('dirija sempre usando o cinto de segurança!')
---
+++ aula-011/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'BR'
+console.log(`vivendo em ${pais}`)
+if(pais == 'BR'){
+    console.log('você é brasileiro')
+}else{
+    console.log('você é estrangeiro')
+}
