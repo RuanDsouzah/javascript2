06a2f7c2a54a35f47bb941acf39ad9dca33a5413
danilosouzah60-svg <195672445+RuanDsouzah@users.noreply.github.com>
2026-03-31 13:02:59 -0300

    laço de repetição FOR

--- aula-013/ambiente.js
+++ aula-013/ambiente.js
@@ -1,12 +1,16 @@
-var c = 1
+/*var c = 1
 do{
     console.log(`passo ${c}`)
     c++
 }while(c <= 6)
 
 
-/*while(c <= 6){
+while(c <= 6){
     console.log(`passo ${c}`)
     c++
 }
 */
+
+for(c = 1; c <= 10; c++){
+    console.log(`passo ${c}`)
+}
